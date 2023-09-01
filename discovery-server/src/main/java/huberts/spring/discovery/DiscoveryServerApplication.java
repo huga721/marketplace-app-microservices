b54407d0bf9752709b40b84e38b2e47abe44c493
huga721 <szabahubert@gmail.com>
2023-09-01 08:32:56 +0200

    add discovery-server service

---
+++ discovery-server/src/main/java/huberts/spring/discovery/DiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package huberts.spring.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+}
