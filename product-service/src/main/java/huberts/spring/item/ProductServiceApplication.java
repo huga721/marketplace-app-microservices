cd2f21d8952b350f402ae12b54bcad456073a6f1
huga721 <szabahubert@gmail.com>
2023-09-20 10:49:16 +0200

    refactor: class refactor

--- product-service/src/main/java/huberts/spring/item/ProductServiceApplication.java
+++ product-service/src/main/java/huberts/spring/item/ProductServiceApplication.java
@@ -2,16 +2,12 @@ package huberts.spring.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//@ComponentScan({"huberts.spring.item.adapter.out.persistance", "huberts.spring.item.common", "huberts.spring.item.domain"})
+@EnableFeignClients
 @SpringBootApplication
 public class ProductServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
     }
-
-//    @Bean
-//    public ProductJpaPort productJpaPort() {
-//        return productJpaPort();
-//    }
 }
