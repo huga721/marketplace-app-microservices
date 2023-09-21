e6c1864234493f5c4da98679c4947a5c2cb2c445
huga721 <szabahubert@gmail.com>
2023-09-21 13:06:05 +0200

    refactor: product request refactor

--- product-service/src/main/java/huberts/spring/item/adapter/in/web/resource/ProductRequest.java
+++ product-service/src/main/java/huberts/spring/item/adapter/in/web/resource/ProductRequest.java
@@ -1,14 +1,10 @@
 package huberts.spring.item.adapter.in.web.resource;
 
-import huberts.spring.item.common.model.Color;
 import huberts.spring.item.common.model.Quality;
-import huberts.spring.item.common.model.Size;
 
 public record ProductRequest(
         String name,
         String description,
         Long price,
-        Color color,
-        Quality quality,
-        Size size) {
+        Quality quality) {
 }
