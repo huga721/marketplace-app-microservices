fe72254edb22acb065ccfab317f7dd3a1cfd4599
huga721 <szabahubert@gmail.com>
2023-09-28 22:51:14 +0200

    add annotation to enable feign clients

--- basket-service/src/main/java/huberts/spring/basket/BasketServiceApplication.java
+++ basket-service/src/main/java/huberts/spring/basket/BasketServiceApplication.java
@@ -2,7 +2,9 @@ package huberts.spring.basket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class BasketServiceApplication {
     public static void main(String[] args) {
