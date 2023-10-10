a5561ee4f62cdc1c51b591576d727c55bb5241f6
huga721 <szabahubert@gmail.com>
2023-10-10 19:17:58 +0200

    refactor basket-service controller

--- basket-service/src/main/java/huberts/spring/basket/adapter/in/web/BasketController.java
+++ basket-service/src/main/java/huberts/spring/basket/adapter/in/web/BasketController.java
@@ -1,24 +1,56 @@
 package huberts.spring.basket.adapter.in.web;
 
-import huberts.spring.basket.domain.port.BasketJpaPort;
-import huberts.spring.basket.domain.port.BasketServicePort;
+import huberts.spring.basket.domain.model.BasketDomainModel;
+import huberts.spring.basket.domain.model.BasketProductDomainModel;
+import huberts.spring.basket.domain.port.in.BasketServicePort;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.security.RolesAllowed;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "baskets", description = "Basket operations")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api")
 public class BasketController {
 
     private final BasketServicePort basketServicePort;
 
-    @PostMapping("/add-product/{productId}")
-    String addProductToBasket(@PathVariable Long productId) {
-        return "xd";
+    @RolesAllowed("role-user")
+    @PostMapping("/products/{productId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    BasketDomainModel addProductToBasket(@PathVariable Long productId,
+                                         @AuthenticationPrincipal Jwt jwt) {
+        String keycloakId = jwt.getSubject();
+        return basketServicePort.addProductToBasket(productId, keycloakId);
+    }
+
+    @RolesAllowed("role-user")
+    @GetMapping("/details")
+    BasketDomainModel getBasketDetails(@AuthenticationPrincipal Jwt jwt) {
+        String keycloakId = jwt.getSubject();
+        return basketServicePort.getBasketDetails(keycloakId);
+    }
+
+    @RolesAllowed("role-user")
+    @GetMapping()
+    List<BasketDomainModel> getBaskets() {
+        return basketServicePort.getBaskets();
+    }
+
+    @RolesAllowed("role-user")
+    @GetMapping("/products")
+    List<BasketProductDomainModel> getBasketProducts() {
+        return basketServicePort.getBasketProducts();
+    }
+
+    @RolesAllowed("role-user")
+    @GetMapping("/{basketId}")
+    BasketDomainModel getBasketById(@PathVariable Long basketId) {
+        return basketServicePort.getBasketById(basketId);
     }
 }
