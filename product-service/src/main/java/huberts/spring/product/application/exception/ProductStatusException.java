adec34601319c16e0980ea720cd6baadb1f201dc
huga721 <szabahubert@gmail.com>
2023-10-11 10:27:29 +0200

    moved package

--- product-service/src/main/java/huberts/spring/product/application/exception/ProductStatusException.java
+++ product-service/src/main/java/huberts/spring/product/application/exception/ProductStatusException.java
@@ -1,4 +1,4 @@
-package huberts.spring.product.common.exception;
+package huberts.spring.product.application.exception;
 
 public class ProductStatusException extends RuntimeException {
     public ProductStatusException(String message) {
