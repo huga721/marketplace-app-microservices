b49faa22fd46d20a0d78cce927e37218ae38097c
huga721 <szabahubert@gmail.com>
2023-11-24 14:40:58 +0100

    Refactored way of injecting keycloak security values

--- user-service/src/main/java/huberts/spring/user/adapter/out/keycloak/config/KeycloakConfig.java
+++ user-service/src/main/java/huberts/spring/user/adapter/out/keycloak/config/KeycloakConfig.java
@@ -1,20 +1,29 @@
 package huberts.spring.user.adapter.out.keycloak.config;
 
+import lombok.RequiredArgsConstructor;
 import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.admin.client.Keycloak;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class KeycloakConfig {
 
-    private final String url = "http://localhost:8181";
-    private final String realm = "marketplace-app-realm";
-    private final String clientId = "marketplace-app";
-    private final String clientSecret = "TehmCIP2NYurYk1J2d8XfszkNYc9ujZg";
-    private final String username = "admin";
-    private final String password = "password";
+    @Value("${keycloak.server-url}")
+    private String SERVER_URL;
+    @Value("${keycloak.realm}")
+    private String REALM;
+    @Value("${keycloak.client-id}")
+    private String CLIENT_ID;
+    @Value("${keycloak.client-secret}")
+    private String CLIENT_SECRET;
+    @Value("${keycloak.username}")
+    private String USERNAME;
+    @Value("${keycloak.password}")
+    private String PASSWORD;
 
     @Bean
     public KeycloakConfigResolver keycloakConfigResolver() {
@@ -23,6 +32,6 @@ public class KeycloakConfig {
 
     @Bean
     public Keycloak keycloak() {
-        return Keycloak.getInstance(url, realm, username, password, clientId, clientSecret);
+        return Keycloak.getInstance(SERVER_URL, REALM, USERNAME, PASSWORD, CLIENT_ID, CLIENT_SECRET);
     }
 }
