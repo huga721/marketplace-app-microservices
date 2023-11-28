4051cea64cefbd8de728c319d2cc7fdf2c253577
huga721 <szabahubert@gmail.com>
2023-11-28 22:40:50 +0100

    Method refactor

--- user-service/src/main/java/huberts/spring/user/adapter/out/persistance/repository/UserRepository.java
+++ user-service/src/main/java/huberts/spring/user/adapter/out/persistance/repository/UserRepository.java
@@ -2,7 +2,12 @@ package huberts.spring.user.adapter.out.persistance.repository;
 
 import huberts.spring.user.adapter.out.persistance.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    boolean existsByUsername(String username);
+
+    Optional<UserEntity> findByKeycloakId(String keycloakId);
 }
--- user-service/src/main/java/huberts/spring/user/domain/port/out/UserJpaPort.java
+++ user-service/src/main/java/huberts/spring/user/domain/port/out/UserJpaPort.java
@@ -1,20 +1,17 @@
 package huberts.spring.user.domain.port.out;
 
-import huberts.spring.user.adapter.in.web.resource.UserRequest;
+import huberts.spring.user.adapter.in.web.resource.CreateRequest;
 import huberts.spring.user.domain.model.UserDomainModel;
 
 import java.util.List;
 
 public interface UserJpaPort {
 
-    UserDomainModel createUser(UserRequest userRequest, String keycloakId);
+    UserDomainModel createUser(CreateRequest userRequest, String keycloakId);
 
     List<UserDomainModel> getAllUsers();
     UserDomainModel getUserById(Long userId);
+    UserDomainModel getUserByKeycloakId(String keycloakId);
 
-    UserDomainModel updateUserById(UserDomainModel userDomainModel);
-
-    void deleteUserById(Long userId);
-
-    boolean existUserByUsername(String username);
+    void deleteUser(UserDomainModel userDomainModel);
 }
