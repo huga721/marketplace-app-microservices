f27c28af8077c1dc7309a40c42dcbab80e1e6dd1
huga721 <szabahubert@gmail.com>
2023-11-28 22:40:15 +0100

    Update and added methods

--- user-service/src/main/java/huberts/spring/user/adapter/out/persistance/UserJpaAdapter.java
+++ user-service/src/main/java/huberts/spring/user/adapter/out/persistance/UserJpaAdapter.java
@@ -1,30 +1,29 @@
 package huberts.spring.user.adapter.out.persistance;
 
-import huberts.spring.user.adapter.in.web.resource.UserRequest;
+import huberts.spring.user.adapter.in.web.resource.CreateRequest;
 import huberts.spring.user.adapter.out.persistance.entity.UserEntity;
 import huberts.spring.user.adapter.out.persistance.repository.UserRepository;
-import huberts.spring.user.application.exception.UserDoesntExistException;
+import huberts.spring.user.application.exception.UserNotFoundException;
 import huberts.spring.user.domain.model.UserDomainModel;
 import huberts.spring.user.domain.port.out.UserJpaPort;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 import static huberts.spring.user.adapter.out.persistance.mapper.UserJpaMapper.USER_JPA_MAPPER;
 
-@Component
+@Service
 @RequiredArgsConstructor
-class UserJpaAdapter implements UserJpaPort {
+public class UserJpaAdapter implements UserJpaPort {
 
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
-
     private final UserRepository userRepository;
 
     @Override
-    public UserDomainModel createUser(UserRequest userRequest, String keycloakId) {
+    public UserDomainModel createUser(CreateRequest userRequest, String keycloakId) {
         UserEntity userEntity = USER_JPA_MAPPER.toJpaEntity(userRequest, keycloakId);
         UserEntity savedUserEntity = userRepository.save(userEntity);
         return USER_JPA_MAPPER.toDomainModel(savedUserEntity);
@@ -42,30 +41,32 @@ class UserJpaAdapter implements UserJpaPort {
         return USER_JPA_MAPPER.toDomainModel(findById(userId));
     }
 
+    @Override
+    public UserDomainModel getUserByKeycloakId(String keycloakId) {
+        return USER_JPA_MAPPER.toDomainModel(findByKeycloakId(keycloakId));
+    }
+
+    private UserEntity findByKeycloakId(String keycloakId) {
+        return userRepository.findByKeycloakId(keycloakId)
+                .orElseThrow(() -> {
+                    String errorMessage = String.format("User with keycloakId = %s doesn't exist", keycloakId);
+                    LOGGER.warn("An exception occurred!", new UserNotFoundException(errorMessage));
+                    return new UserNotFoundException(errorMessage);
+                });
+    }
+
     private UserEntity findById(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> {
                     String errorMessage = String.format("User with id = %s doesn't exist", userId);
-                    LOGGER.warn("An exception occurred!", new UserDoesntExistException(errorMessage));
-                    return new UserDoesntExistException(errorMessage);
+                    LOGGER.warn("An exception occurred!", new UserNotFoundException(errorMessage));
+                    return new UserNotFoundException(errorMessage);
                 });
     }
 
     @Override
-    public UserDomainModel updateUserById(UserDomainModel userDomainModel) {
-        UserEntity userEntity = USER_JPA_MAPPER.toUpdatedJpaEntity(userDomainModel);
-        UserEntity savedUserEntity = userRepository.save(userEntity);
-        return USER_JPA_MAPPER.toDomainModel(savedUserEntity);
-    }
-
-    @Override
-    public void deleteUserById(Long userId) {
-        UserEntity userEntity = findById(userId);
-        userRepository.delete(userEntity);
-    }
-
-    @Override
-    public boolean existUserByUsername(String username) {
-        return userRepository.existsByUsername(username);
+    public void deleteUser(UserDomainModel userDomainModel) {
+        UserEntity user = USER_JPA_MAPPER.toUpdatedJpaEntity(userDomainModel);
+        userRepository.delete(user);
     }
 }
