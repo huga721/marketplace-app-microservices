b21e06a6b6c1b674050b6a4f4306e8af725fde29
huga721 <szabahubert@gmail.com>
2023-11-29 21:10:42 +0100

    Removed transactional annotation and implemented getBasketById method

--- basket-service/src/main/java/huberts/spring/basket/adapter/out/persistance/BasketJpaAdapter.java
+++ basket-service/src/main/java/huberts/spring/basket/adapter/out/persistance/BasketJpaAdapter.java
@@ -3,10 +3,13 @@ package huberts.spring.basket.adapter.out.persistance;
 import huberts.spring.basket.adapter.out.persistance.entity.BasketEntity;
 import huberts.spring.basket.adapter.out.persistance.repository.BasketRepository;
 import huberts.spring.basket.adapter.out.feign.product.model.Status;
+import huberts.spring.basket.application.exception.BasketNotFoundException;
 import huberts.spring.basket.domain.model.BasketDomainModel;
 import huberts.spring.basket.domain.port.out.BasketJpaPort;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -17,10 +20,10 @@ import static huberts.spring.basket.adapter.out.persistance.mapper.BasketJpaMapp
 @RequiredArgsConstructor
 public class BasketJpaAdapter implements BasketJpaPort {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(BasketJpaAdapter.class);
     private final BasketRepository basketRepository;
 
     @Override
-    @Transactional
     public BasketDomainModel saveBasket(BasketDomainModel basketDomainModel) {
         BasketEntity basketEntity = BASKET_JPA_MAPPER.toJpaEntity(basketDomainModel);
         BasketEntity basketSaved = basketRepository.save(basketEntity);
@@ -28,20 +31,23 @@ public class BasketJpaAdapter implements BasketJpaPort {
     }
 
     @Override
-    @Transactional
     public BasketDomainModel findBasketByKeycloakIdAndStatus(String keycloakId, Status status) {
-        BasketEntity basketEntity = basketRepository.findBasketEntityByKeycloakIdAndStatus(
-                keycloakId, status);
+        BasketEntity basketEntity = basketRepository.findBasketEntityByKeycloakIdAndStatus(keycloakId, status);
         return BASKET_JPA_MAPPER.toDomainModel(basketEntity);
     }
 
     @Override
-    public BasketDomainModel findBasketById(Long basketId) {
-        return null;
+    public BasketDomainModel getBasketById(Long basketId) {
+        BasketEntity basket = basketRepository.findById(basketId)
+                .orElseThrow(() -> {
+                    String errorMessage = String.format("Basket with id = %s doesn't exist.", basketId);
+                    LOGGER.warn("An exception occurred!", new BasketNotFoundException(errorMessage));
+                    return new BasketNotFoundException(errorMessage);
+                });
+        return BASKET_JPA_MAPPER.toDomainModel(basket);
     }
 
     @Override
-    @Transactional
     public List<BasketDomainModel> getAllBaskets() {
         return basketRepository.findAll().stream()
                 .map(BASKET_JPA_MAPPER::toDomainModel)
