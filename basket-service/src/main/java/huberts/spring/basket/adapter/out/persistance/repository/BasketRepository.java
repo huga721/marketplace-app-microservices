4cdccff759fc81b453ea738bbc68895aa1c01223
huga721 <szabahubert@gmail.com>
2023-12-01 23:46:53 +0100

    Added repository annotation

--- basket-service/src/main/java/huberts/spring/basket/adapter/out/persistance/repository/BasketRepository.java
+++ basket-service/src/main/java/huberts/spring/basket/adapter/out/persistance/repository/BasketRepository.java
@@ -3,7 +3,9 @@ package huberts.spring.basket.adapter.out.persistance.repository;
 import huberts.spring.basket.adapter.out.persistance.entity.BasketEntity;
 import huberts.spring.basket.adapter.out.feign.product.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BasketRepository extends JpaRepository<BasketEntity, Long> {
     BasketEntity findBasketEntityByKeycloakIdAndStatus(String keycloakId, Status status);
 }
