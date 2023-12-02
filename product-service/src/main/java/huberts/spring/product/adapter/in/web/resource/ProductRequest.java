16fdc32f45dfb0edefb270ee1a8776402f37e73e
huga721 <szabahubert@gmail.com>
2023-12-02 23:14:01 +0100

    Added jakarta validation annotations

--- product-service/src/main/java/huberts/spring/product/adapter/in/web/resource/ProductRequest.java
+++ product-service/src/main/java/huberts/spring/product/adapter/in/web/resource/ProductRequest.java
@@ -1,8 +1,12 @@
 package huberts.spring.product.adapter.in.web.resource;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record ProductRequest(
-        String name,
-        String description,
-        Long price,
-        Quality quality) {
+        @NotBlank(message = "Field \"name\" can not be blank.") String name,
+        @NotBlank(message = "Field \"description\" can not be blank.") String description,
+        @Min(value = 1, message = "Price should be not less than 1.") Long price,
+        @NotNull(message = "Field \"quality\" can not be blank.") Quality quality) {
 }
