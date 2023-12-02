3d95cac5a979c20bea80fbc908e9f59b98e6b772
huga721 <szabahubert@gmail.com>
2023-12-02 23:13:49 +0100

    Implemented methods

--- product-service/src/main/java/huberts/spring/product/adapter/out/persistance/repository/ProductRepository.java
+++ product-service/src/main/java/huberts/spring/product/adapter/out/persistance/repository/ProductRepository.java
@@ -1,8 +1,16 @@
 package huberts.spring.product.adapter.out.persistance.repository;
 
 import huberts.spring.product.adapter.out.persistance.entity.ProductEntity;
+import huberts.spring.product.adapter.out.persistance.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-//    void deleteById(Long productId)
+    Optional<ProductEntity> findByIdAndKeycloakId(Long id, String keycloakId);
+    List<ProductEntity> findAllByKeycloakIdAndStatus(String keycloakId, Status status);
+    List<ProductEntity> findAllByStatus(Status status);
 }
