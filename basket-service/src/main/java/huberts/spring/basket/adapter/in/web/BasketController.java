e6160208e97e884fe08e9f34d3ecb2a1743df885
huga721 <szabahubert@gmail.com>
2023-12-06 11:39:52 +0100

    Refactored BasketController, added logs, implemented removeProductFromBasket method

--- basket-service/src/main/java/huberts/spring/basket/adapter/in/web/BasketController.java
+++ basket-service/src/main/java/huberts/spring/basket/adapter/in/web/BasketController.java
@@ -6,54 +6,67 @@ import huberts.spring.basket.domain.port.in.BasketServicePort;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.security.RolesAllowed;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Tag(name = "baskets", description = "Basket operations")
+@Tag(name = "basket", description = "Basket REST API")
+@Validated
 @RestController
 @RequestMapping("/api/basket")
 @RequiredArgsConstructor
 public class BasketController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(BasketController.class);
     private final BasketServicePort basketServicePort;
 
     @GetMapping()
     List<BasketDomainModel> getBaskets() {
-        List<BasketDomainModel> baskets = basketServicePort.getBaskets();
-        return baskets;
+        LOGGER.info(">> BasketController: getting all baskets");
+        return basketServicePort.getBaskets();
     }
 
     @GetMapping("/{basketId}")
     BasketDomainModel getBasketById(@PathVariable Long basketId) {
-        BasketDomainModel basket = basketServicePort.getBasketById(basketId);
-        return basket;
+        LOGGER.info(">> BasketController: getting basket by id: {}", basketId);
+        return basketServicePort.getBasketById(basketId);
     }
 
-    @GetMapping("/products")
+    @GetMapping("/product")
     List<BasketProductDomainModel> getBasketProducts() {
-        List<BasketProductDomainModel> basketProducts = basketServicePort.getBasketProducts();
-        return basketProducts;
+        LOGGER.info(">> BasketController: getting all basket products");
+        return basketServicePort.getBasketProducts();
     }
 
     @RolesAllowed("role-user")
     @GetMapping("/details")
     BasketDomainModel getBasketDetails(@AuthenticationPrincipal Jwt jwt) {
+        LOGGER.info(">> BasketController: getting basket details of current authenticated user");
         String keycloakId = jwt.getSubject();
-        BasketDomainModel basket = basketServicePort.getBasketDetails(keycloakId);
-        return basket;
+        return basketServicePort.getBasketDetails(keycloakId);
     }
 
     @RolesAllowed("role-user")
     @PostMapping("/product/{productId}")
     @ResponseStatus(HttpStatus.CREATED)
-    BasketDomainModel addProductToBasket(@PathVariable Long productId,
-                                         @AuthenticationPrincipal Jwt jwt) {
+    BasketDomainModel addProductToBasket(@PathVariable Long productId, @AuthenticationPrincipal Jwt jwt) {
+        LOGGER.info(">> BasketController: adding product with id: {} to basket", productId);
         String keycloakId = jwt.getSubject();
-        BasketDomainModel basket = basketServicePort.addProductToBasket(productId, keycloakId);
-        return basket;
+        return basketServicePort.addProductToBasket(productId, keycloakId);
+    }
+
+    @RolesAllowed("role-user")
+    @DeleteMapping("/product/{productId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    String removeProductFromBasket(@PathVariable Long productId, @AuthenticationPrincipal Jwt jwt) {
+        LOGGER.info(">> BasketController: removing product with id: {} from basket", productId);
+        String keycloakId = jwt.getSubject();
+        return basketServicePort.removeProductFromBasket(productId, keycloakId);
     }
 }
