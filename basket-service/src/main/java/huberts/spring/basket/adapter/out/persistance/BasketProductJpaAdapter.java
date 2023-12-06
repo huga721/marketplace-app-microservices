c8f9cce52d60b91082f21546901027d7a9bbe639
huga721 <szabahubert@gmail.com>
2023-12-06 11:41:41 +0100

    Implemented deleteBaskedProduct, refactored methods

--- basket-service/src/main/java/huberts/spring/basket/adapter/out/persistance/BasketProductJpaAdapter.java
+++ basket-service/src/main/java/huberts/spring/basket/adapter/out/persistance/BasketProductJpaAdapter.java
@@ -2,15 +2,11 @@ package huberts.spring.basket.adapter.out.persistance;
 
 import huberts.spring.basket.adapter.out.persistance.entity.BasketEntity;
 import huberts.spring.basket.adapter.out.persistance.entity.BasketProductEntity;
-import huberts.spring.basket.adapter.out.persistance.mapper.BasketProductJpaMapper;
 import huberts.spring.basket.adapter.out.persistance.repository.BasketProductRepository;
 import huberts.spring.basket.domain.model.BasketDomainModel;
 import huberts.spring.basket.domain.model.BasketProductDomainModel;
 import huberts.spring.basket.domain.port.out.BasketProductJpaPort;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -19,16 +15,14 @@ import static huberts.spring.basket.adapter.out.persistance.mapper.BasketJpaMapp
 import static huberts.spring.basket.adapter.out.persistance.mapper.BasketProductJpaMapper.BASKET_PRODUCT_JPA_MAPPER;
 
 @Component
-@Transactional
 @RequiredArgsConstructor
 public class BasketProductJpaAdapter implements BasketProductJpaPort {
 
     private final BasketProductRepository basketProductRepository;
 
     @Override
-    @Transactional
-    public BasketProductDomainModel createBasketProduct(BasketProductDomainModel basketProduct,
-                                                        BasketDomainModel basketDomainModel) {
+    public BasketProductDomainModel saveBasketProduct(BasketProductDomainModel basketProduct,
+                                                      BasketDomainModel basketDomainModel) {
         BasketEntity basket = BASKET_JPA_MAPPER.toJpaEntity(basketDomainModel);
         BasketProductEntity basketProductEntity = BASKET_PRODUCT_JPA_MAPPER.toJpaEntity(basketProduct, basket);
         BasketProductEntity basketProductSaved = basketProductRepository.save(basketProductEntity);
@@ -36,11 +30,16 @@ public class BasketProductJpaAdapter implements BasketProductJpaPort {
     }
 
     @Override
-    @Transactional
     public List<BasketProductDomainModel> getAllBasketProducts() {
-        List<BasketProductDomainModel> basketProducts = basketProductRepository.findAll().stream()
+        return basketProductRepository.findAll().stream()
                 .map(BASKET_PRODUCT_JPA_MAPPER::toDomainModel)
                 .toList();
-        return basketProducts;
+    }
+
+    @Override
+    public void deleteBasketProduct(BasketProductDomainModel basketProductDomainModel, BasketDomainModel basketDomainModel) {
+        BasketEntity basketEntity = BASKET_JPA_MAPPER.toJpaEntity(basketDomainModel);
+        BasketProductEntity basketProductEntity = BASKET_PRODUCT_JPA_MAPPER.toJpaEntity(basketProductDomainModel, basketEntity);
+        basketProductRepository.delete(basketProductEntity);
     }
 }
