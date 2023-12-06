dcb6e884de9decd47901105431cf6dc645bd984f
huga721 <szabahubert@gmail.com>
2023-12-06 11:44:30 +0100

    Added toDomainModel mapper method and refactored toJpaEntity mapper method

--- product-service/src/main/java/huberts/spring/product/adapter/out/persistance/mapper/ProductJpaMapper.java
+++ product-service/src/main/java/huberts/spring/product/adapter/out/persistance/mapper/ProductJpaMapper.java
@@ -1,10 +1,8 @@
 package huberts.spring.product.adapter.out.persistance.mapper;
 
-import huberts.spring.product.adapter.in.web.resource.ProductRequest;
 import huberts.spring.product.adapter.out.persistance.entity.ProductEntity;
 import huberts.spring.product.domain.model.ProductDomainModel;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -14,8 +12,5 @@ public interface ProductJpaMapper {
 
     ProductDomainModel toDomainModel(ProductEntity productEntity);
 
-    @Mapping(target = "status", constant = "ACTIVE")
-    @Mapping(target = "keycloakId", source = "keycloakId")
-    ProductEntity toJpaEntity(ProductRequest productRequest, String keycloakId);
-
+    ProductEntity toJpaEntity(ProductDomainModel productDomain);
 }
