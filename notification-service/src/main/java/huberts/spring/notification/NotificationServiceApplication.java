c698edd03e756b03f5c11e92f2cafc110199986b
huga721 <szabahubert@gmail.com>
2023-12-07 14:24:02 +0100

    Added starter class

---
+++ notification-service/src/main/java/huberts/spring/notification/NotificationServiceApplication.java
@@ -0,0 +1,10 @@
+package huberts.spring.notification;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
