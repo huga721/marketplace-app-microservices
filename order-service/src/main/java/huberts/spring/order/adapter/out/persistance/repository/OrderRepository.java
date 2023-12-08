88283938e8f42706170c42bd88781f537c052467
huga721 <szabahubert@gmail.com>
2023-12-09 00:02:30 +0100

    Added findByKeycloakId method declaration

--- order-service/src/main/java/huberts/spring/order/adapter/out/persistance/repository/OrderRepository.java
+++ order-service/src/main/java/huberts/spring/order/adapter/out/persistance/repository/OrderRepository.java
@@ -4,6 +4,10 @@ import huberts.spring.order.adapter.out.persistance.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+    Optional<List<OrderEntity>> findByKeycloakId(String keycloakId);
 }
