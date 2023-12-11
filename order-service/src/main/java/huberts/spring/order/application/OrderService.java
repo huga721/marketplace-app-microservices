b65f01b3a88eb0b2063e5556a369d3238cfc9be0
huga721 <szabahubert@gmail.com>
2023-12-11 20:41:24 +0100

    Implemented getOrderById and getAuthenticatedUserOrder, refactored class

--- order-service/src/main/java/huberts/spring/order/application/OrderService.java
+++ order-service/src/main/java/huberts/spring/order/application/OrderService.java
@@ -8,6 +8,8 @@ import huberts.spring.order.domain.port.in.KafkaServicePort;
 import huberts.spring.order.domain.port.in.OrderServicePort;
 import huberts.spring.order.domain.port.out.OrderJpaPort;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,12 +18,16 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderService implements OrderServicePort {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(OrderService.class);
+
     private final OrderJpaPort orderJpaPort;
     private final KafkaServicePort kafkaServicePort;
     private final BasketFeignClient basketFeignClient;
 
     @Override
     public OrderDomainModel createOrder(OrderRequest orderRequest, String keycloakId) {
+        LOGGER.info(">> OrderService: creating order of basket user with keycloak id: {}", keycloakId);
+
         BasketDomainModel basket = basketFeignClient.getBasketDetails();
 
         OrderDomainModel order = new OrderDomainModel();
@@ -32,12 +38,28 @@ public class OrderService implements OrderServicePort {
         order.setPostalCode(orderRequest.deliveryAddress().postalCode());
 
         OrderDomainModel orderSaved = orderJpaPort.saveOrder(order);
+
         kafkaServicePort.sendBasketInactiveEvent(basket);
+        kafkaServicePort.sendNotificationMessage(basket);
+
+        LOGGER.info(">> OrderService: order created");
         return orderSaved;
     }
 
     @Override
     public List<OrderDomainModel> getOrders() {
+        LOGGER.info(">> OrderService: getting all orders");
         return orderJpaPort.getAllOrders();
+    }
+
+    @Override
+    public OrderDomainModel getOrderById(Long orderId) {
+        LOGGER.info(">> OrderService: getting order with id: {}", orderId);
+        return orderJpaPort.getOrderById(orderId);
+    }
+
+    @Override
+    public List<OrderDomainModel> getAuthenticatedUserOrder(String keycloakId) {
+        return orderJpaPort.getOrdersByKeycloakId(keycloakId);
     }
 }
