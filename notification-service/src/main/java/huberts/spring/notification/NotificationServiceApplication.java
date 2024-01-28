5f968c3a4dab2d7214f7fd5e355056d02ad536a5
huga721 <szabahubert@gmail.com>
2024-01-28 13:53:42 +0100

    Add SpringBootApplication run method

--- notification-service/src/main/java/huberts/spring/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/huberts/spring/notification/NotificationServiceApplication.java
@@ -1,10 +1,11 @@
 package huberts.spring.notification;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(NotificationServiceApplication.class, args);
     }
 }
