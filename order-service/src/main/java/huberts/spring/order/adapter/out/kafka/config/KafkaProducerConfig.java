8a5ed9a4ee2d8aa16ad95b7a33ad03d0470d8558
huga721 <szabahubert@gmail.com>
2024-01-29 17:11:37 +0100

    refactor: implemented annotation value injection in constructor

--- order-service/src/main/java/huberts/spring/order/adapter/out/kafka/config/KafkaProducerConfig.java
+++ order-service/src/main/java/huberts/spring/order/adapter/out/kafka/config/KafkaProducerConfig.java
@@ -2,6 +2,7 @@ package huberts.spring.order.adapter.out.kafka.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -14,11 +15,15 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
-    private String bootstrapServers = "localhost:9092";
+    private final String BOOTSTRAP_SERVER;
+
+    public KafkaProducerConfig(@Value("${spring.kafka.bootstrap-servers}") String bootstrapServer) {
+        this.BOOTSTRAP_SERVER = bootstrapServer;
+    }
 
     public Map<String, Object> producerConfig() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return props;
