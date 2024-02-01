4572d4363a928e0e3c7704d8d1fc87a5106cef78
huga721 <szabahubert@gmail.com>
2024-02-01 18:17:12 +0100

    delete due to changes in message logic

--- order-service/src/main/java/huberts/spring/order/domain/port/in/KafkaServicePort.java
+++
@@ -1,8 +0,0 @@
-package huberts.spring.order.domain.port.in;
-
-import huberts.spring.order.adapter.out.feign.basket.model.BasketDomainModel;
-
-public interface KafkaServicePort {
-
-    void sendBasketInactiveEvent(BasketDomainModel basketDomainModel);
-}
