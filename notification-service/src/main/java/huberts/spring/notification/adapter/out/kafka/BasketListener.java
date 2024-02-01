fedd4c3c1f24651d1c7ae198fd4f07740f170700
huga721 <szabahubert@gmail.com>
2024-02-01 18:11:47 +0100

    implemented listener methods

--- notification-service/src/main/java/huberts/spring/notification/adapter/out/kafka/BasketListener.java
+++ notification-service/src/main/java/huberts/spring/notification/adapter/out/kafka/BasketListener.java
@@ -1,8 +1,9 @@
 package huberts.spring.notification.adapter.out.kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import huberts.spring.notification.adapter.out.kafka.model.BasketModel;
 import huberts.spring.notification.domain.port.in.NotificationServicePort;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,19 +11,54 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
-@Getter
 @RequiredArgsConstructor
 public class BasketListener {
 
     private final NotificationServicePort notificationServicePort;
-
-    private final Logger LOGGER = LoggerFactory.getLogger(BasketListener.class);
+    private final Logger LOGGER = LoggerFactory.getLogger(OrderListener.class);
     private final ObjectMapper objectMapper;
 
-    @KafkaListener(topics = "order.notification", groupId = "one")
-    public String listens(final String in) {
-        LOGGER.info(">> KafkaListener: received message from topic = order.notification");
-//        notificationServicePort.createNotification(in);
-        return in;
+    @KafkaListener(topics = "basket.create.notification", groupId = "notification")
+    public void listenBasketCreateEvent(final String in) throws JsonProcessingException {
+        LOGGER.info(">> BasketListener: caught event on topic = basket.create.notification {}", in);
+        BasketModel basket = objectMapper.readValue(in, BasketModel.class);
+        String keycloakId = basket.keycloakId();
+        String message = "Basket with id: " + basket.id() + " was created.";
+        notificationServicePort.createNotification(message, keycloakId);
+    }
+
+    @KafkaListener(topics = "basket.add.product.notification", groupId = "notification")
+    public void listenBasketAddProductEvent(final String in) throws JsonProcessingException {
+        LOGGER.info(">> BasketListener: caught event on topic = basket.add.product.notification {}", in);
+        BasketModel basket = objectMapper.readValue(in, BasketModel.class);
+        String keycloakId = basket.keycloakId();
+
+        int indexLastProduct = basket.basketProducts().size() - 1;
+        Long productId = basket.basketProducts().get(indexLastProduct).id();
+
+        String message = "Product with id: " + productId + " was added to basket with id: " + basket.id();
+        notificationServicePort.createNotification(message, keycloakId);
+    }
+
+    @KafkaListener(topics = "basket.remove.product.notification", groupId = "notification")
+    public void listenBasketRemoveProductEvent(final String in) throws JsonProcessingException {
+        LOGGER.info(">> BasketListener: caught event on topic = basket.remove.product.notification {}", in);
+        BasketModel basket = objectMapper.readValue(in, BasketModel.class);
+        String keycloakId = basket.keycloakId();
+
+        int indexLastProduct = basket.basketProducts().size() - 1;
+        Long productId = basket.basketProducts().get(indexLastProduct).id();
+
+        String message = "Product with id: " + productId + " was remove from basket with id: " + basket.id();
+        notificationServicePort.createNotification(message, keycloakId);
+    }
+
+    @KafkaListener(topics = "basket.inactive.notification", groupId = "notification")
+    public void listenBasketInactiveProductEvent(final String in) throws JsonProcessingException {
+        LOGGER.info(">> BasketListener: caught event on topic = basket.inactive.notification {}", in);
+        BasketModel basket = objectMapper.readValue(in, BasketModel.class);
+        String keycloakId = basket.keycloakId();
+        String message = "Basket with id: " + basket.id() + " is inactive.";
+        notificationServicePort.createNotification(message, keycloakId);
     }
 }
--- notification-service/src/main/java/huberts/spring/notification/adapter/out/kafka/ProductListener.java
+++ notification-service/src/main/java/huberts/spring/notification/adapter/out/kafka/ProductListener.java
@@ -1,6 +1,7 @@
 package huberts.spring.notification.adapter.out.kafka;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import huberts.spring.notification.adapter.out.kafka.model.ProductModel;
 import huberts.spring.notification.domain.port.in.NotificationServicePort;
@@ -10,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class ProductListener {
@@ -18,12 +21,42 @@ public class ProductListener {
     private final ObjectMapper objectMapper;
     private final NotificationServicePort notificationServicePort;
 
-    @KafkaListener(topics = "topic.created.notification", groupId = "one")
+    @KafkaListener(topics = "product.created.notification", groupId = "notification")
     public void listenProductCreatedEvent(final String in) throws JsonProcessingException {
         LOGGER.info(">> ProductListener: caught event on topic = topic.created.notification {}", in);
         ProductModel product = objectMapper.readValue(in, ProductModel.class);
         String keycloakId = product.keycloakId();
         String message = "Product with id: " + product.id() + " was created.";
+        notificationServicePort.createNotification(message, keycloakId);
+    }
+
+    @KafkaListener(topics = "product.inactive.notification", groupId = "notification")
+    public void listenProductInactiveEvent(final String in) throws JsonProcessingException {
+        LOGGER.info(">> ProductListener: caught event on topic = topic.created.notification {}", in);
+        List<ProductModel> products = objectMapper.readValue(in, new TypeReference<>() {});
+
+        products.forEach(product -> {
+            String keycloakId = product.keycloakId();
+            String message = "Product with id: " + product.id() + " was set to inactive due to sold of item.";
+            notificationServicePort.createNotification(message, keycloakId);
+        });
+    }
+
+    @KafkaListener(topics = "product.delete.notification", groupId = "notification")
+    public void listenProductDeleteEvent(final String in) throws JsonProcessingException {
+        LOGGER.info(">> ProductListener: caught event on topic = topic.delete.notification {}", in);
+        ProductModel product = objectMapper.readValue(in, ProductModel.class);
+        String keycloakId = product.keycloakId();
+        String message = "Product with id: " + product.id() + " was deleted.";
+        notificationServicePort.createNotification(message, keycloakId);
+    }
+
+    @KafkaListener(topics = "product.edit.notification", groupId = "notification")
+    public void listenProductEditEvent(final String in) throws JsonProcessingException {
+        LOGGER.info(">> ProductListener: caught event on topic = topic.edit.notification {}", in);
+        ProductModel product = objectMapper.readValue(in, ProductModel.class);
+        String keycloakId = product.keycloakId();
+        String message = "Product with id: " + product.id() + " was edited.";
         notificationServicePort.createNotification(message, keycloakId);
     }
 }
