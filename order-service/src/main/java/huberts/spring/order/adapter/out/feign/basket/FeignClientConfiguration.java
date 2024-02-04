b5563d3c02ba0b11de9243b843eea2a30155dd28
huga721 <szabahubert@gmail.com>
2024-02-04 22:42:59 +0100

    add feign client configuration and exception message model

---
+++ order-service/src/main/java/huberts/spring/order/adapter/out/feign/basket/FeignClientConfiguration.java
@@ -0,0 +1,38 @@
+package huberts.spring.order.adapter.out.feign.basket;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import feign.codec.ErrorDecoder;
+import huberts.spring.order.application.exception.FeignAuthorizationException;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+@RequiredArgsConstructor
+public class FeignClientConfiguration implements RequestInterceptor {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(FeignClientConfiguration.class);
+
+    @Override
+    public void apply(RequestTemplate template) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof Jwt) {
+            Jwt jwt = (Jwt) authentication.getPrincipal();
+            template.header("Authorization", "Bearer " + jwt.getTokenValue());
+            LOGGER.info("Added authorization header to Feign requestTemplate");
+        } else {
+            String errorMessage = "Unable to add Authoriation header to Feign requestTemplate";
+            LOGGER.error("An exception occurred!", new FeignAuthorizationException(errorMessage));
+            throw new FeignAuthorizationException(errorMessage);
+        }
+    }
+
+    @Bean
+    ErrorDecoder errorDecoder() {
+        return new BasketClientErrorDecoder();
+    }
+}
